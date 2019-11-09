be070b3c5eec9e6e904835a75f412de353f9b0e6
renatoabreu14 <renato.abreu.info@gmail.com>
2019-11-09 16:57:50 -0300

    Versão após o TechTalkAndroid #3

--- app/src/main/java/com/example/techtalkandroid/MainActivity.java
+++ app/src/main/java/com/example/techtalkandroid/MainActivity.java
@@ -2,25 +2,45 @@ package com.example.techtalkandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnLogin;
+    private Button btnCadastrar;
+    private EditText email;
+    private EditText senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        email = (EditText)findViewById(R.id.txtEmail);
+        senha = (EditText)findViewById(R.id.txtSenha);
+
         btnLogin = (Button)findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Toast.makeText(MainActivity.this, "Preencha os campos", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, MarketPlaceActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnCadastrar = (Button)findViewById(R.id.btnCadastrar);
+        btnCadastrar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //Toast.makeText(MainActivity.this, "Direcionando para a tela de cadastro", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/com/example/techtalkandroid/MarketPlaceActivity.java
@@ -0,0 +1,14 @@
+package com.example.techtalkandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MarketPlaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_market_place);
+    }
+}
